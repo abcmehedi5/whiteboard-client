d29e50d20f1afc9249e04f5513466b5d5b836cca
abcmehedi5 <abcmehedi5@gmail.com>
2023-12-25 10:06:46 +0600

    active class added

--- src/Components/Drawing/DrawingToolbar.jsx
+++ src/Components/Drawing/DrawingToolbar.jsx
@@ -1,17 +1,24 @@
 import { FaPencilAlt } from "react-icons/fa";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { FaTextHeight } from "react-icons/fa";
+import { useState } from "react";
 
 const DrawingToolbar = ({ setActiveTool, setColor }) => {
+  // local state for active tool
+  const [activeTool, setActiveToolState] = useState("");
+
   const handleSetActiveTool = (tool) => {
     setActiveTool(tool);
+    setActiveToolState(tool);
   };
 
   return (
-    <div className=" top-0 left-0 p-4 bg-stone-100 shadow-md w-full">
-      <div className="flex justify-around ">
+    <div className="top-0 left-0 p-4 bg-stone-100 shadow-md w-full">
+      <div className="flex justify-around">
         <button
-          className="mr-2 p-2 border rounded flex items-center gap-2"
+          className={`mr-2 p-2 border rounded flex items-center gap-2 ${
+            activeTool === "pencil" ? "bg-blue-200" : ""
+          }`} // conditionally apply style for active tool
           onClick={() => handleSetActiveTool("pencil")}
         >
           <span>
@@ -19,7 +26,9 @@ const DrawingToolbar = ({ setActiveTool, setColor }) => {
           </span>
         </button>
         <button
-          className="mr-2 p-2 border rounded flex items-center gap-2"
+          className={`mr-2 p-2 border rounded flex items-center gap-2 ${
+            activeTool === "rectangle" ? "bg-blue-200" : ""
+          }`} // conditionally apply style for active tool
           onClick={() => handleSetActiveTool("rectangle")}
         >
           <span>
@@ -27,7 +36,9 @@ const DrawingToolbar = ({ setActiveTool, setColor }) => {
           </span>
         </button>
         <button
-          className="mr-2 p-2 border rounded flex items-center gap-2"
+          className={`mr-2 p-2 border rounded flex items-center gap-2 ${
+            activeTool === "text" ? "bg-blue-200" : ""
+          }`} // conditionally apply style for active tool
           onClick={() => handleSetActiveTool("text")}
         >
           <span>
